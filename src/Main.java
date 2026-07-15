5a0c6261f5028ac0028e697a594d6c85ee8f9b8e
senthilvadivel venkatesan <senthilvadivelvenkatesan@mac.lan>
2026-07-15 13:37:25 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+
+        // This is my first Java program.
+        /*
+            Another way to write "System.out.print"
+            You can say "sout" then hit tab.
+         */
+        System.out.print("I like pizza!");
+        System.out.print("It's really good!");
+        System.out.print("yipee!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Suhaas says hi count"+ i );
+        }
+    }
+}
